c03781b603dd8f8ce397ae8012a4184a312197a1
Ritik jagpal <ritikjagpal@gmail.com>
2019-10-19 19:11:21 -0400

    36

--- C0762067_F2019_MID_TERM_MAD3004/main.swift
+++ C0762067_F2019_MID_TERM_MAD3004/main.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 
-var i = Internet(ibillid: "In001", pname: "Roggers", internetused: 22.2, bid: 1, bammount: 25, bdate: "06/26/2019", btype: Bill.bill_Type.internet)
-var m = Mobile(mbillid: "Mb1001", modelname: "Apple", mnumber: 4745962475, Gbused: 25.2, minutesused: 258.3, bid: 1, bammount: 256.0, bdate: "09/25/2019", btype: Bill.bill_Type.mobile)
- i.printData()
-m.printData()
+var int1 = Internet(ibillid: "In001", pname: "Roggers", internetused: 22.2, bid: 1, bammount: 25, bdate: "06/26/2019", btype: Bill.bill_Type.internet)
+var mobile1 = Mobile(mbillid: "Mb1001", modelname: "Apple", mnumber: 4745962475, Gbused: 25.2, minutesused: 258.3, bid: 1, bammount: 256.0, bdate: "09/25/2019", btype: Bill.bill_Type.mobile)
+var hydro1 = Hydro(hBillId: "hydro1001", agenceyname: "Toronto Electricity ", hydroconsuption: 258.23, bid: 1, bammount: 2563.2, bdate: "06/25/2019", btype: Bill.bill_Type.hydro)
