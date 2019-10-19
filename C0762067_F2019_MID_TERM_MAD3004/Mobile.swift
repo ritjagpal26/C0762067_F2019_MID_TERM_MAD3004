65819441eddacea57ad35856df91044c04d63051
Ritik jagpal <ritikjagpal@gmail.com>
2019-10-19 18:03:44 -0400

    6

---
+++ C0762067_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -0,0 +1,14 @@
+//
+//  Mobile.swift
+//  C0762067_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Mobile: Bill {
+    var mobile_Bill_Id = String()
+    var mobile_Model_Name = String()
+  
+}
