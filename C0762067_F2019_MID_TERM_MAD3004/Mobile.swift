fb5fdc503dda662d74f6ddccd3287105b2999d7a
Ritik jagpal <ritikjagpal@gmail.com>
2019-10-19 18:07:05 -0400

    12

--- C0762067_F2019_MID_TERM_MAD3004/Mobile.swift
+++ C0762067_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -14,4 +14,8 @@ class Mobile: Bill {
     var  internet_GB_used = Float()
     var minute_used = Float()
     var mobile_Bill_Ammount = Float()
+    init(mbillid:String,modelname: String, mnumber : Int, igu: Float, mu: Float, mba: Float,bid: Int, bdate: Date, btype:bill_Type) {
+        self.mobile_Bill_Id = mbillid
+        self.mobile_Model_Name = modelname
+        self.mobile_number = mnumber
 }
